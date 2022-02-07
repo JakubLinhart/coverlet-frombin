e77f3855f1fddacb7032ec7951cbdc3c004dff53
Jakub Linhart <jakub.linhart@gmail.com>
2022-02-07 23:51:25 +0100

    remove specific files

--- nocoverage.cmd
+++ nocoverage.cmd
@@ -1,9 +1,12 @@
 git clean -dxf
-git reset --hard
 
 dotnet build
-cd ClassLibrary1
-git clean -dxf
-cd ..
+
+REM If any of these files are missing, then no code coverage data are collected.
+
+REM del ClassLibrary1\obj\Debug\netstandard2.1\ClassLibrary1.dll
+REM del ClassLibrary1\obj\Debug\netstandard2.1\ClassLibrary1.pdb
+REM del ClassLibrary1\obj\Debug\netstandard2.1\ClassLibrary1.AssemblyInfo.cs
+del ClassLibrary1\obj\Debug\netstandard2.1\.NETStandard,Version=v2.1.AssemblyAttributes.cs
 
 dotnet test TestProject1 --collect:"XPlat Code Coverage" --no-build --diag:log.txt
