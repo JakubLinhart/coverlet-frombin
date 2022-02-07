53a362a7d6d9376d6f23d95041ee47d31bba895f
Jakub Linhart <jakub.linhart@gmail.com>
2022-02-08 00:15:21 +0100

    add a comment

--- nocoverage.cmd
+++ nocoverage.cmd
@@ -2,6 +2,7 @@ git clean -dxf
 
 dotnet build
 
+REM After removing this single file, no coverge data are collected.
 del ClassLibrary1\obj\Debug\netstandard2.1\ClassLibrary1.pdb
 
 dotnet test TestProject1 --collect:"XPlat Code Coverage" --no-build --diag:log.txt
