77e2c6b1dfa3b59dca8999e7075ca5ec2a51269a
Jakub Linhart <jakub.linhart@gmail.com>
2022-02-07 23:58:28 +0100

    nocoverage fix

--- nocoverage.cmd
+++ nocoverage.cmd
@@ -2,11 +2,6 @@ git clean -dxf
 
 dotnet build
 
-REM If any of these files are missing, then no code coverage data are collected.
-
-REM del ClassLibrary1\obj\Debug\netstandard2.1\ClassLibrary1.dll
-REM del ClassLibrary1\obj\Debug\netstandard2.1\ClassLibrary1.pdb
-REM del ClassLibrary1\obj\Debug\netstandard2.1\ClassLibrary1.AssemblyInfo.cs
-del ClassLibrary1\obj\Debug\netstandard2.1\.NETStandard,Version=v2.1.AssemblyAttributes.cs
+del ClassLibrary1\obj\Debug\netstandard2.1\ClassLibrary1.pdb
 
 dotnet test TestProject1 --collect:"XPlat Code Coverage" --no-build --diag:log.txt
